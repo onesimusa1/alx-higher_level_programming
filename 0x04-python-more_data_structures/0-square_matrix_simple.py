0bc1058829bbd051c52fa4568bde426d75798e0d
onesimusa1 <onesimusa1@gmail.com>
2022-12-08 01:22:44 +0100

    Task 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    """A function that computes the square value. Use Map and Lambda"""
-    return list(map(lambda x: list(map(lambda y: y * y, x)), matrix))
+    return ([list(map(lambda x: x ** 2, i))for i in matrix])
